298e3f00e4511147a476f2ea589a1c1218d14fbd
Vsnikhilmaheswar <nikhilmaheswar12@gmail.com>
2025-04-11 21:19:43 +0530

    added 3d model

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import heroImage from "./assets/hero.jpg"; // Adjust path as needed
 import Nav from "./Nav"; // Ensure Nav is correctly imported
+import Spline from '@splinetool/react-spline';
+
 import {
   Github,
   Linkedin,
@@ -71,8 +73,14 @@ function Home() {
                 
                 </div>
            </div>
-           <div className="hidden md:flex justify-center  p-10 md:p-3">
-                   <img className="rounded-3xl bg-cover w-155 h-120 md:w-150 md:h-120" src="https://img.freepik.com/free-vector/online-games-concept_23-2148527136.jpg?t=st=1743571847~exp=1743575447~hmac=c80d0fc83a8da77fab27def43353166ac3bcb45f6f4933ac981b52974f46d63e&w=1380"/>           </div>
+           <div className="hidden md:flex justify-center   ">
+                   {/* <img className="rounded-3xl bg-cover w-155 h-120 md:w-150 md:h-120" src="https://img.freepik.com/free-vector/online-games-concept_23-2148527136.jpg?t=st=1743571847~exp=1743575447~hmac=c80d0fc83a8da77fab27def43353166ac3bcb45f6f4933ac981b52974f46d63e&w=1380"/>     */}
+                   <main className="mt-0">
+      <Spline
+        scene="https://prod.spline.design/4hnOz5Al-jhCp5LV/scene.splinecode" 
+      />
+    </main>
+                          </div>
 
     </div>
     </div>
